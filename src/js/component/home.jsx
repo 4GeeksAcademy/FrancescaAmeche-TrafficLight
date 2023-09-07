5f3e9e07077a0764ac86f9b364b1a2b2686a2810
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-09-07 16:45:54 +0000

    traffic light styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import TrafficLight from "./trafficlight";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<TrafficLight/>
 		</div>
 	);
 };
--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -2,40 +2,40 @@ import React, {useState} from "react";
 
 const TrafficLight = () => {
 
-    let [trafficLightRed, settrafficLightRed] = useState("red");
-    let [trafficLightYellow, settrafficLightYellow] = useState("yellow");
-    let [trafficLightGreen, settrafficLightGreen] = useState("green");
+    let [trafficLightRed, settrafficLightRed] = useState("none");
+    let [trafficLightYellow, settrafficLightYellow] = useState("none");
+    let [trafficLightGreen, settrafficLightGreen] = useState("none");
 
     function red () {
-        if (trafficLightRed == "black") {
-            settrafficLightRed("red")
+        if (trafficLightRed == "0px 0px 30px 20px rgba(255, 255, 190, .75)") {
+            settrafficLightRed("none")
         }
         else {
-            settrafficLightRed("black")
-            settrafficLightYellow("yellow")
-            settrafficLightGreen("green")
+            settrafficLightRed("0px 0px 30px 20px rgba(255, 255, 190, .75")
+            settrafficLightYellow("none")
+            settrafficLightGreen("none")
         }
         console.log("red");
     };
     function yellow () {
-        if (trafficLightYellow == "black") {
-            settrafficLightYellow("yellow")
+        if (trafficLightYellow == "0px 0px 30px 20px rgba(255, 255, 190, .75") {
+            settrafficLightYellow("none")
         }
         else {
-            settrafficLightYellow("black")
-            settrafficLightRed("red")
-            settrafficLightGreen("green")
+            settrafficLightYellow("0px 0px 30px 20px rgba(255, 255, 190, .75")
+            settrafficLightRed("none")
+            settrafficLightGreen("none")
         }
         console.log("yellow");
     };
     function green () {
-        if (trafficLightGreen == "black") {
-            settrafficLightGreen("green")
+        if (trafficLightGreen == "0px 0px 30px 20px rgba(255, 255, 190, .75") {
+            settrafficLightGreen("none")
         }
         else {
-            settrafficLightGreen("black")
-            settrafficLightYellow("yellow")
-            settrafficLightRed("red")
+            settrafficLightGreen("0px 0px 30px 20px rgba(255, 255, 190, .75")
+            settrafficLightYellow("none")
+            settrafficLightRed("none")
         }
         console.log("green");
     };
@@ -43,13 +43,12 @@ const TrafficLight = () => {
 
     return (
         <div className="trafficLight d-flex flex-column justify-content-center ms-5">
-            <div className="trafficTop bg-black ms-5" style={{width: "10px", height: "100px"}}></div>
+            <div className="trafficTop bg-black ms-5 mt-3" style={{width: "10px", height: "100px"}}></div>
             <div className="lights d-flex flex-column bg-black rounded" style={{width: "100px", height: "300px"}}>
-                <button className="rounded-circle bg-danger" style={{height: "100px", borderColor: trafficLightRed}}onClick={red}></button>
-                <button className="rounded-circle bg-warning" style={{height: "100px", borderColor: trafficLightYellow}}onClick={yellow}></button>
-                <button className="rounded-circle bg-success" style={{height: "100px", borderColor: trafficLightGreen}}onClick={green}></button>
+                <button className="rounded-circle bg-danger" style={{height: "100px", boxShadow: trafficLightRed}}onClick={red}></button>
+                <button className="rounded-circle bg-warning" style={{height: "100px", boxShadow: trafficLightYellow}}onClick={yellow}></button>
+                <button className="rounded-circle bg-success" style={{height: "100px", boxShadow: trafficLightGreen}}onClick={green}></button>
             </div>
-
         </div>
     );
 };
